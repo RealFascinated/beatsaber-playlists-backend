5c4da8890966290bc7f57a19ee7a8b650f87c4c1
RealFascinated <67254223+RealFascinated@users.noreply.github.com>
2022-05-02 18:04:29 +0100

    Removed leftover files from EJS testing

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import 'dotenv/config'; // import dotenv and set environment variables
 
 const app = express();
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
 // import the routing files
 import Routes from './route/routes';
 Routes(app);
